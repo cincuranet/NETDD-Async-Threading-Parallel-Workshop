ed77a232711d08477842703b786ca33883e91248
Jiri Cincura <jiri@cincura.net>
2023-10-26 11:50:26 +0200

    Progress

--- Workshop/Workshop/Program.cs
+++ Workshop/Workshop/Program.cs
@@ -13,21 +13,27 @@ namespace Workshop
             //});
             ////Task.Factory.StartNew(() => { }, )
 
-            var arr = Enumerable.Range(0, 1024).ToArray();
-            var sw = Stopwatch.StartNew();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = Sum(arr[i], arr[i]);
-            }
-            Console.WriteLine(sw.Elapsed);
+            //var arr = Enumerable.Range(0, 1024).ToArray();
+            //var sw = Stopwatch.StartNew();
+            //var tasks = new List<Task>();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    var local = i;
+            //    tasks.Add(Task.Run(() =>
+            //    {
+            //        arr[local] = Sum(arr[local], arr[local]);
+            //    }));
+            //}
+            //Task.WaitAll(tasks.ToArray());
+            //Console.WriteLine(sw.Elapsed);
 
             Console.ReadLine();
         }
 
-        static int Sum(int a, int b)
-        {
-            Thread.SpinWait(5*99999);
-            return a + b;
-        }
+        //static int Sum(int a, int b)
+        //{
+        //    Thread.SpinWait(5*99999);
+        //    return a + b;
+        //}
     }
 }
