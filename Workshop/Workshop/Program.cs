7dcf8b9be87a905985e5d6b461c92df02cb49257
Jiri Cincura <jiri@cincura.net>
2023-10-26 10:17:52 +0200

    Progress

---
+++ Workshop/Workshop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var t = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Test");
+            //    return 10;
+            //});
+            ////Task.Factory.StartNew(() => { }, )
+
+            var arr = Enumerable.Range(0, 1024).ToArray();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Sum(arr[i], arr[i]);
+            }
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadLine();
+        }
+
+        static int Sum(int a, int b)
+        {
+            Thread.SpinWait(5*99999);
+            return a + b;
+        }
+    }
+}
