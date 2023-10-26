935dbf9bc2775b044b551f1fa913710b4db27afb
Jiri Cincura <jiri@cincura.net>
2023-10-26 12:33:36 +0200

    Progress

--- Workshop/Workshop/Program.cs
+++ Workshop/Workshop/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System.Diagnostics;
+using System.Net;
 
 namespace Workshop
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //static Task Main()
+        //{
+        //    Console.WriteLine();
+        //    return Task.CompletedTask.ContinueWith(_ =>
+        //    {
+        //        Console.WriteLine();
+        //        Task.CompletedTask.ContinueWith(_ =>
+        //        {
+        //            Console.WriteLine();
+        //        });
+        //    });
+        //}
+
+        static void await(object o) { }
+
+        static async Task Main(string[] args)
         {
+            #region 
             //var t = Task.Run(() =>
             //{
             //    Console.WriteLine("Test");
@@ -26,8 +43,70 @@ namespace Workshop
             //}
             //Task.WaitAll(tasks.ToArray());
             //Console.WriteLine(sw.Elapsed);
+            #endregion
+
+            var request = WebRequest.CreateHttp("https://www.tabsoverspaces.com");
+            using (var response = await request.GetResponseAsync())
+            {
+                using (var ms = new MemoryStream((int)response.ContentLength))
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        var buffer = new byte[4096];
+                        while (true)
+                        {
+                            try
+                            {
+                                var read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                                if (read == 0)
+                                    break;
+                                await ms.WriteAsync(buffer, 0, read);
+                            }
+                            catch (IOException)
+                            {
+                                Console.WriteLine("ERROR");
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("HERE");
 
             Console.ReadLine();
+        }
+
+        static void ReadStream(Stream source, Stream destination, Action onDone)
+        {
+            var buffer = new byte[4096];
+            source.BeginRead(buffer, 0, buffer.Length, ar =>
+            {
+                int read;
+                try
+                {
+                    read = source.EndRead(ar);
+                }
+                catch (IOException)
+                {
+                    return;
+                }
+                if (read == 0)
+                {
+                    onDone();
+                    return;
+                }
+                destination.BeginWrite(buffer, 0, read, ar2 =>
+                {
+                    try
+                    {
+                        destination.EndWrite(ar2);
+                    }
+                    catch (IOException)
+                    {
+                        return;
+                    }
+                    ReadStream(source, destination, onDone);
+                }, null);
+            }, null);
         }
 
         //static int Sum(int a, int b)
